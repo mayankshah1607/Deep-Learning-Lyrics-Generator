6740f07c5406071088b5740379b0e290d5332aca
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2018-09-09 18:13:31 +0530

    Minor fix

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def predict():
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	print('Starting app...')
-    app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
 
