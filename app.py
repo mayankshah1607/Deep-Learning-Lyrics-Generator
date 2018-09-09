6e56272d4ec9ae77ca04568261344050dcde289c
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2018-09-09 17:36:11 +0530

    Updated port and Procfile

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def predict():
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000)
+	port = int(os.environ.get("PORT", 5000))
+	print('Starting app...')
     app.run(host='0.0.0.0', port=port)
 
